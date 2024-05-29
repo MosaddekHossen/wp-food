56a0508f7e24cba3cef5bb79a323a4dd3006f47d
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-05-29 22:51:26 +0600

    update footer

--- inc/template-parts/footer/footer.php
+++ inc/template-parts/footer/footer.php
@@ -1,21 +1,21 @@
 <?php if (is_active_sidebar('footer-sidebar-1') or is_active_sidebar('footer-sidebar-2') or is_active_sidebar('footer-sidebar-3')) : ?>
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-4">
-                    <?php dynamic_sidebar('footer-sidebar-1'); ?>
-                </div>
-                <div class="col-sm-4">
-                    <?php dynamic_sidebar('footer-sidebar-2'); ?>
-                </div>
-                <div class="col-sm-4">
-                    <?php dynamic_sidebar('footer-sidebar-3'); ?>
-                </div>
+    <!-- page footer  -->
+    <div class="container-fluid bg-dark text-light has-height-md middle-items border-top text-center wow fadeIn">
+        <div class="row">
+            <div class="col-sm-4">
+                <?php dynamic_sidebar('footer-sidebar-1'); ?>
+            </div>
+            <div class="col-sm-4">
+                <?php dynamic_sidebar('footer-sidebar-2'); ?>
+            </div>
+            <div class="col-sm-4">
+                <?php dynamic_sidebar('footer-sidebar-3'); ?>
             </div>
         </div>
-    </footer>
+    </div>
+    <!-- end of page footer -->
 <?php endif; ?>
 
-<div class="sub-footer">
-    <?php food_copyright_option(); ?>
+<div class="bg-dark text-light text-center border-top wow fadeIn">
+    <p class="mb-0 py-3 text-muted small"><?php food_copyright_option(); ?></a></p>
 </div>
